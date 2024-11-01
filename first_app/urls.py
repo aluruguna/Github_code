ffab4aeb255b342ce935e0febb27c28950996c58
aluruguna <aluruguna@gmail.com>
2024-11-01 13:21:12 -0700

    static_files (#3)
    Co-authored-by: gunasekhar aluru <gunasekharaluru@MacBook-Pro.local>

--- first_app/urls.py
+++ first_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from first_app import views
 
 urlpatterns = [
-    path('', views.home, name="home"),
+    path('', views.home, name="index"),
     path('about/', views.about, name='about'),
     path('search/', views.search,name="search"),
 ]
