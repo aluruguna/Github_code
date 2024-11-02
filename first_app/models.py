cbec11aaec545f797be27328b376a780adae8541
gunasekhar aluru <gunasekharaluru@MacBook-Pro.local>
2024-11-02 12:23:55 -0700

    OneToOne

--- first_app/models.py
+++ first_app/models.py
@@ -23,3 +23,26 @@ class AccessRecord(models.Model):
 
     def __str__(self):
         return str(self.date)
+
+class Company(models.Model):
+    name = models.CharField(max_length=264, unique=True)
+    number_of_employees = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+
+class Employee(models.Model):
+    employee_name = models.CharField(max_length=264, unique=True)
+    company_name = models.ForeignKey(Company, on_delete=models.CASCADE)
+    date_of_birth = models.DateField(default= date.today)
+
+    def __str__(self):
+        return self.employee_name
+
+class Project(models.Model):
+    project_name = models.CharField(max_length=264, unique= True)
+    employee_name = models.ManyToManyField('Employee')
+    # team_lead = models.OneToOneField('Employee', on_delete=models.CASCADE, related_name='team_lead')
+
+    def __str__(self):
+        return self.project_name
